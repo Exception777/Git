0fa1bfd7cbf9a57c09805a24367f543fafa59f84
ylwei <weiyalun777@163.com>
2022-06-29 21:42:21 +0800

    commit

--- GitTest/src/main/java/com/atguigu/git/GitTest.java
+++ GitTest/src/main/java/com/atguigu/git/GitTest.java
@@ -13,6 +13,7 @@ public class GitTest {
         System.out.println("pull git test");
         System.out.println("pull git test");
         System.out.println("pull git test");
+        System.out.println("pull git test111");
     }
 
 
