e74419b036c6ade5863eefdc00d04144c60475ca
Exception777 <108388416+Exception777@users.noreply.github.com>
2022-06-29 21:30:32 +0800

    Update GitTest.java
    pull git test

--- GitTest/src/main/java/com/atguigu/git/GitTest.java
+++ GitTest/src/main/java/com/atguigu/git/GitTest.java
@@ -9,6 +9,7 @@ public class GitTest {
         System.out.println("hello git");
         System.out.println("master git test");
         System.out.println("hello git");
+        System.out.println("pull git test");
     }
 
 
